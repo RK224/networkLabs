940f5c9dc1dbe11f78294eaa3d9ad707b08849af
RK224 <rahulkhapre1998@gmail.com>
2019-04-12 16:25:54 +0530

    lab3

--- client.c
+++ client.c
@@ -2,54 +2,56 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
 #define BUFSIZE 32
 
-int main(){
+int main(void){
     int sock = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
     if(sock < 0){
-        printf("Error in opening socket\n");
+        printf("Error in opening the socket\n");
         exit(0);
     }
-    printf("socket created\n");
+    printf("Client Socket created\n");
 
     struct sockaddr_in serverAddr;
-    memset(&serverAddr,0,sizeof(serverAddr));
+    memset(&serverAddr,0,sizeof(struct sockaddr_in));
 
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(12345);
     serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     printf("Address assigned\n");
 
-    int c = connect(sock,(struct sockaddr*) &serverAddr,sizeof(serverAddr));
+    int c = connect(sock,(struct sockaddr*) &serverAddr, sizeof(serverAddr));
     printf("%d\n",c);
     if(c < 0){
-        printf("Error while establishing a connection\n");
+        printf("Error while establishing connection\n");
         exit(0);
     }
     printf("Connection established\n");
 
-    printf("ENTER MESSAGE FOR SERVER with max 32 characters:\n");
-    char* msg = (char*)malloc(sizeof(char)*BUFSIZE);
+    printf("Enter message for the server with max 32 characters\n");
+    char msg[BUFSIZE];
     fgets(msg,BUFSIZE,stdin);
-    int bytesent = send(sock,msg,strlen(msg),0);
-    if(bytesent != strlen(msg)){
-        printf("Error while sending message\n");
+    int bytesSent = send(sock,msg,strlen(msg),0);
+    if(bytesSent != strlen(msg)){
+        printf("Error while sending the message\n");
         exit(0);
     }
     printf("Data sent\n");
 
-    char* recvBuffer = (char*)malloc(sizeof(char)*BUFSIZE);
+    char recvBuffer[BUFSIZE];
     int bytesRecvd = recv(sock,recvBuffer,BUFSIZE-1,0);
-    if(bytesRecvd < 0){
+    if(bytesRecvd < 0 ){
         printf("Error while receiving data from the server\n");
         exit(0);
     }
-    recvBuffer[bytesRecvd] = '\0';
+
+    recvBuffer[bytesRecvd] ='\0';
     printf("%s\n",recvBuffer);
+    
     close(sock);
-    free(msg);
-    free(recvBuffer);
     return 0;
 }
--- server.c
+++ server.c
@@ -2,46 +2,60 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
 #define MAXPENDING 5
 #define BUFFERSIZE 32
 
+void upperCase(char* msg){
+    int n = strlen(msg);
+    for(int i=0;i<n;i++){
+        msg[i] = toupper(msg[i]);
+    }
+}
+
+void roundUp(char* msg){
+    int d = (int)ceil(atof(msg));
+    sprintf(msg,"%d",d);
+}
+
 int main(void){
-    int serverSocket = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
-    if(serverSocket < 0 ){
-        printf("Error while server socket creation\n");
+    int serverSocket = socket(PF_INET,SOCK_STREAM,0);
+    if(serverSocket < 0){
+        printf("Error while creating server socket\n");
         exit(0);
     }
     printf("Server socket created\n");
 
     struct sockaddr_in serverAddress,clientAddress;
-    memset(&serverAddress,0,sizeof(serverAddress));
+    memset(&serverAddress,0,sizeof(struct sockaddr_in));
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(12345);
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_addr.s_addr = htons(INADDR_ANY);
     printf("Server address assigned\n");
 
-    int temp = bind(serverSocket,(struct sockaddr*) &serverAddress,sizeof(serverAddress));
+    int temp = bind(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)); 
     if(temp < 0){
         printf("Error while binding\n");
         exit(0);
-    }   
-    printf("Binding succesful\n");
+    }
+    printf("Binding successful\n");
     
     int temp1 = listen(serverSocket,MAXPENDING);
-    if(temp1 < 0){
-        printf("Error in listening\n");
+    if(temp1 < 0 ){
+        printf("Error in listen\n");
         exit(0);
     }
     printf("Now listening\n");
-
+    
     char msg[BUFFERSIZE];
-
     int clientLength = sizeof(clientAddress);
-    int clientSocket = accept(serverSocket,(struct  sockaddr*) &clientAddress,&clientLength);
+    int clientSocket = accept(serverSocket,(struct sockaddr*) &clientAddress,&clientLength);
 
-    if(clientLength < 0){
+    if(clientLength < 0 ){
         printf("Error in client socket\n");
         exit(0);
     }
@@ -49,19 +63,19 @@ int main(void){
 
     int temp2 = recv(clientSocket,msg,BUFFERSIZE,0);
     if(temp2 < 0){
-        printf("Problem in temp 2\n");
+        printf("Problem in temp2\n");
         exit(0);
     }
-    
     msg[temp2] = '\0';
     printf("%s\n",msg);
-    printf("ENTER MESSAGE FOR CLIENT:\n");
-    fgets(msg,BUFFERSIZE,stdin);
+
+   roundUp(msg);    
+
     int bytesSent = send(clientSocket,msg,strlen(msg),0);
     if(bytesSent != strlen(msg)){
-        printf("Error while sending message to the server\n");
+        printf("Error while sending messages\n");
         exit(0);
     }
     close(serverSocket);
-    return 0;
 }
+
